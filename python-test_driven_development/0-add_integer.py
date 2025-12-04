681ef47f7283109561b85ff05cc3cb3c581d2be1
AleidBanan <Aleidbanan@gmail.com>
2025-12-04 14:03:29 +0000

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+"""
+This module has one function that adds up 2 integers
+"""
+
+
 def add_integer(a, b=98):
-    """Function return the result of addition of a and b
+    """Return the sum of two integers or floats as integers
     Args:
-        a(int) -> The first arg, must be passed
-        b(int) -> The second arg, if not passed, 98 is assumed to be the value
-    Returns an int
+        a: first argument
+        b: second argument
+    Returns:
+        Sum of the two arguments
+    Raises:
+        TypeError: If either of the arguments not an integer or a float
     """
-    try:
-        if not (isinstance(a, int)) and not isinstance(a, float):
-            raise TypeError("a must be an integer")
-        elif not (isinstance(b, int)) and not isinstance(b, float):
-            raise TypeError("b must be an integer")
-        else:
-            if isinstance(a, float) or isinstance(b, float):
-                return (int(a) + int(b))
-            return a + b
-    except Exception as e:
-        return (e)
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
