4c93a10d04f8d1a9cc7a45c7e26a1519cec08106
AleidBanan <Aleidbanan@gmail.com>
2025-12-04 13:13:56 +0000

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """
-Module for add_integer function.
+Module for add_integer function
 """
 
-
 def add_integer(a, b=98):
-    """
-    Add two integers or floats and return the result as an integer.
-
-    a and b must be integers or floats; otherwise a TypeError is raised.
-    Floats are cast to integers before addition.
-    """
+    """Adds two integers or floats, after casting them to integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    a = int(a)
+    b = int(b)
+
+    return a + b
