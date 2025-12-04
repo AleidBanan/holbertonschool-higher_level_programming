493be80c4a8484e891a5600c13e6caac3b415e9f
AleidBanan <Aleidbanan@gmail.com>
2025-12-04 13:48:39 +0000

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,10 +4,10 @@ Module for add_integer function
 """
 
 def add_integer(a, b=98):
-    """Adds two integers or floats, after casting them to integers."""
+    """Return the integer addition of a and b."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return (int(a) + int(b))
+    return int(a + b)
