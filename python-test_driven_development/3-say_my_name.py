4bf61c4be0031e7f195f097faaaec6e91e8ddbf7
AleidBanan <Aleidbanan@gmail.com>
2025-12-06 07:36:18 +0000

    task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-module 3-say_my_name
-Contain one method that print first and last name
+Module 3-say_my_name
+Contains one method that prints the first and last name
 """
 
+
 def say_my_name(first_name, last_name=""):
-    """print full name"""
-    message = "first_name must be a string or last_name must be a string"
+    """Print full name."""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
