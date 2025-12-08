3c1303c7f9f07bad1535746cb03880d3cf02e440
AleidBanan <Aleidbanan@gmail.com>
2025-12-08 16:19:14 +0000

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# 2-square.py
+"one-line module docstring"
+
+class Square:
+    "one-line class docstring"
+    def __init__(self, size=0):
+        # validation checks
+        # assign private attribute
