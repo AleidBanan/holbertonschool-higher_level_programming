a3e5a5da85b143560e8c9938af33010d8303bf89
AleidBanan <Aleidbanan@gmail.com>
2025-12-08 16:09:59 +0000

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 # 1-square.py
+"""
+This is a class module
+that creates a class called
+Square.
+"""
 
-"""Defines a Square class."""
+
 class Square:
-    """Represents a square."""
+    """This is the class Square
+    that has a private instance method
+    private instance method are created
+    with __attribute(variable)name
+
+    """
     def __init__(self, size):
+        """
+        this is a built-in special python
+        function that help instantiate object/
+        instance of class variables it is also
+        called an initialization method
+        """
         self.__size = size
