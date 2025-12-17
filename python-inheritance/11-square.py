cf9fcf7250566e46f596f36da054eb484be46770
AleidBanan <Aleidbanan@gmail.com>
2025-12-17 16:30:06 +0000

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module: Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize the square with size as private attribute"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the string representation of the square"""
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
