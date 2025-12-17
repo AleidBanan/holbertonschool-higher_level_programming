92df8ccb35e8cd3a7c18c2b746654fe5769f9c40
AleidBanan <Aleidbanan@gmail.com>
2025-12-17 16:21:21 +0000

    7-base_geometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Module: BaseGeometry class"""
+"""Module 7-base_geometry: define add public instance method"""
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
+    """class basegeometry use public instance method"""
 
     def area(self):
+        """raise an Exception with message"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validates value"""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
