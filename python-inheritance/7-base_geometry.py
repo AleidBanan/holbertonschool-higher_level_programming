9f334b76209cca137c26a1276c9831aa79c852c1
AleidBanan <Aleidbanan@gmail.com>
2025-12-18 09:23:42 +0000

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""Module 7-base_geometry: define add public instance method"""
+'''7-base_geometry.py
+Module that contain an empty class'''
 
 
-class BaseGeometry:
-    """class basegeometry use public instance method"""
+class BaseGeometry():
+    '''Creating an empty class with an empty method'''
 
     def area(self):
-        """raise an Exception with message"""
+        '''Raising an exception'''
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value"""
-        if not isinstance(value, int):
+        '''Raising exception if the integer is not valid'''
+        if value is None:
             raise TypeError(f"{name} must be an integer")
-        if value <= 0:
+        elif type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+        else:
+            return True
