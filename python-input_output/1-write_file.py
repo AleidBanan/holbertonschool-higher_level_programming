4087c7fed01dd00b092de426ab17c8c388065ccd
AleidBanan <Aleidbanan@gmail.com>
2025-12-22 15:24:55 +0000

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
-"""Module that provides a function to write text to a file."""
-
-
 def write_file(filename="", text=""):
-    """Writes a string to a UTF-8 text file and returns the number of characters written."""
-    with open(filename, "w", encoding="utf-8") as file:
-        return file.write(text)
+    """Write a string to a UTF-8 text file and return number of characters written."""
+    with open(filename, "w", encoding="utf-8") as f:  # file is created or truncated
+        nb_chars = f.write(text)  # returns number of characters written [web:7]
+    return nb_chars
