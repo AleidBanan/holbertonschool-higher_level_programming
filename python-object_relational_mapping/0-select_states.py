fde4d11fe8e2f63dad3d6a9acd60ad7368fe635c
AleidBanan <Aleidbanan@gmail.com>
2026-01-17 11:12:40 +0000

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,45 +1,25 @@
 #!/usr/bin/python3
-
-"""
-Lists all states from the database hbtn_0e_0_usa
-"""
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""Lists all states from a MySQL database"""
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
-    # Create a cursor to execute SQL queries
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], host="localhost", port=3306,
-                         charset="utf8")
-    cur = db.cursor()
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id ASC;")
 
-    # Execute the query to select all states sorted by id ascending
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch and display all rows
-    rows = cur.fetchall()
+    rows = c.fetchall()
     for row in rows:
         print(row)
 
-    # Close cursor and database connection
-    for state in cur.fetchall():
-        print(state)
-    cur.close()
+    c.close()
     db.close()
