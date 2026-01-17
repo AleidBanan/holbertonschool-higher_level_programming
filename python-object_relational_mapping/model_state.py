ba79275618bb7bd7617514eb262e8995f35c2b25
AleidBanan <Aleidbanan@gmail.com>
2026-01-17 12:19:41 +0000

    model_state.p

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Defines the City class."""
+"""Creates the states table in the database."""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
 
 
-class City(Base):
-    """City class mapped to the cities table."""
-    __tablename__ = "cities"
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
